12497fa8d11bba590817b545aa4fd27417b6886d
Jakub Misiura <jakubmi9@gmail.com>
2016-10-17 20:28:17 +0200

    remade io subsystem

--- projekt 1/projekt 1/projekt 1.cpp
+++ projekt 1/projekt 1/projekt 1.cpp
@@ -6,8 +6,18 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv [])
 {
+	//crypt.exe -encode||-decode||-help -src="/*sciezka wejscia*/ -code="/*sciezka do tablicy kodujacej*/" -o="/*sciezka wyjscia*/"
+	if(argv[1]=="-help")
+	{
+		printf("crypt help utility\n\
+usage:\n\
+crypt -encode -src=\"\" -code=\"\" -o=\"\" encodes \'src\' with \'code\' and writes it to \'o\'\n\
+crypt -decode -src=\"\" -code=\"\" -o=\"\" decodes \'src\' with \'code\' and writes it to \'o\'\n\
+crypt -help displays this help message");
+		return 0;
+	}
 	ifstream* txt;
 	txt = new ifstream;
 	txt->open("original.txt");
