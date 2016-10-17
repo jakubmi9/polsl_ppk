79b822f5337545784a48bc13db6de170385ba137
Student <Student@A-KOMPUTER>
2016-10-17 11:23:28 +0200

    wyszukiwanie

--- Temat02/Temat02/Temat02.cpp
+++ Temat02/Temat02/Temat02.cpp
@@ -12,7 +12,31 @@ double delta(double a, double b, double c)
 
 int main()
 {
-	double a, b, c;
+	srand(time(0));
+	const int tabsize = 100;
+	int tab[tabsize];
+	for(int i =0;i<tabsize;i++)
+	{
+		tab[i] = rand();
+	}
+	int i = 0;
+	int szukana;
+	cin >> szukana;
+	bool fuse = 1;
+	while (fuse && i < tabsize)
+	{
+		if (tab[i] == szukana)
+		{
+			printf("Szukana liczba to %d, znajduje sie w komorce nr %d\n", szukana, i);
+			fuse = 0;
+		}
+	}
+	if (fuse)
+	{
+		printf("Liczby nie ma w tablicy\n");
+	}
+	/*
+	double a=0, b=0, c=0;
 	do
 	{
 		printf("Podaj wspolczynnik a rozny od zera\n");
@@ -22,6 +46,7 @@ int main()
 	printf("Podaj wspolczynnik b\n");
 	cin>>b;
 	printf("Podaj wspolczynnik c\n");
+
 	cin>>c;
 	if (delta(a, b, c) > 0)
 	{
@@ -38,6 +63,7 @@ int main()
 		double x0 = -b / 2 * a;
 		printf("Rownanie ma jeden pierwiastek rowny %fl\n", x0);
 	}
+	*/
 	system("pause");
     return 0;
 }
