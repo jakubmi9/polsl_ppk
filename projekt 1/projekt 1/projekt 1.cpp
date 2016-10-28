fd1ee4083ccd326119137cd45125d2da6bb38fbf
Jakub Misiura <jakubmi9@gmail.com>
2016-10-28 22:55:20 +0200

    removed references to classes, main() remade to use switch{case:}

--- projekt 1/projekt 1/projekt 1.cpp
+++ projekt 1/projekt 1/projekt 1.cpp
@@ -2,7 +2,6 @@
 //
 
 #include "stdafx.h"
-#include "cipher.h"
 
 //char code[128];
 
@@ -23,12 +22,12 @@ void generate()
 	{
 		if(i<32||i==127)
 		{
-//			code[i] = 0;
+			//			code[i] = 0;
 			ciph<<'\0'<<endl;
 		}
 		else
 		{
-//			code[i] = cipher[i-32];
+			//			code[i] = cipher[i-32];
 			ciph<<cipher[i-32]<<endl;
 		}
 	}
@@ -59,7 +58,7 @@ void encode(char* i, char* o)
 		printf("Couldn't find the source file!\nExiting...");
 		return;
 	}
-	
+
 }
 
 void decode(char* i, char* o)
@@ -89,102 +88,94 @@ void decode(char* i, char* o)
 int main(int argc, char* argv[])
 {
 	//crypt.exe -encode||-decode||-help -src="/*sciezka wejscia*/ -code="/*sciezka do tablicy kodujacej*/" -o="/*sciezka wyjscia*/"
-	if(argv[1][1]=='h')
+	ifstream cfile;
+	switch(argv[1][1])
 	{
-		printf("crypt help utility\n\
-usage:\n\
-crypt -generate generates new random codetable\
-crypt -encode -src=\"\" -o=\"\" encodes \'src\' with code.txt and writes it to \'o\'\n\
-crypt -decode -src=\"\" -o=\"\" decodes \'src\' with code.txt and writes it to \'o\'\n\
-crypt -help displays this help message");
-		system("pause");
-		return 0;
-	}
-	else if(argv[1][1]=='g')
-	{
-		printf("Generating code.txt...\n");
-		generate();
-	}
-	else if(argv[1][1]=='e')
-	{
-		char iname[260] = {0};
-		char oname[260] = {0};
-		{
-			int j = 0;
-			int i = 5;
-			while(argv[2][i]!='\0')
-			{
-				iname[j] = argv[2][i];
-				i++;
-				j++;
-			}
-		}
-		{
-			int j = 0;
-			int i = 6;
-			while(argv[3][i]!='\0')
-			{
-				oname[j] = argv[3][i];
-				i++;
-				j++;
-			}
-		}
-		ifstream cfile;
-		cfile.open("code.txt");
-		if(cfile.is_open())
-		{
-
-			encode(iname, oname);
-		}
-		else
-		{
-			printf("Couldn't find code.txt!\nPlease run crypt -generate first.\nExiting...");
+		case 'h':
+			printf("crypt help utility\n\
+					usage:\n\
+					crypt -generate generates new random codetable\
+					crypt -encode -src=\"\" -o=\"\" encodes \'src\' with code.txt and writes it to \'o\'\n\
+					crypt -decode -src=\"\" -o=\"\" decodes \'src\' with code.txt and writes it to \'o\'\n\
+					crypt -help displays this help message");
+			system("pause");
 			return 0;
-		}
-		
-	}
-	else if(argv[1][1]=='d')
-	{
-		char iname[260] = {0};
-		char oname[260] = {0};
-		{
-			int j = 0;
-			int i = 5;
-			while(argv[2][i]!='\0')
-			{
-				iname[j] = argv[2][i];
-				i++;
-				j++;
-			}
-		}
-		{
-			int j = 0;
-			int i = 6;
-			while(argv[3][i]!='\0')
-			{
-				oname[j] = argv[3][i];
-				i++;
-				j++;
-			}
-		}
-		ifstream cfile;
-		cfile.open("code.txt");
-		if(cfile.is_open())
-		{
-			decode(iname, oname);
-		}
-		else
-		{
-			printf("Couldn't find code.txt!\nPlease run crypt -generate first.\nExiting...");
+		case 'g':
+			printf("Generating code.txt...\n");
+			generate();
 			return 0;
-		}
-		
-	}
-	else
-	{
-		printf("Invalid runtime argument!\nExiting...");
-		return 0;
+		case 'e':
+			char iname[260] = {0};
+			char oname[260] = {0};
+			{
+				int j = 0;
+				int i = 5;
+				while(argv[2][i]!='\0')
+				{
+					iname[j] = argv[2][i];
+					i++;
+					j++;
+				}
+			}
+			{
+				int j = 0;
+				int i = 6;
+				while(argv[3][i]!='\0')
+				{
+					oname[j] = argv[3][i];
+					i++;
+					j++;
+				}
+			}
+			cfile.open("code.txt");
+			if(cfile.is_open())
+			{
+				encode(iname, oname);
+				return 0;
+			}
+			else
+			{
+				printf("Couldn't find code.txt!\nPlease run crypt -generate first.\nExiting...");
+				return 0;
+			}
+		case 'd':
+			char iname[260] = {0};
+			char oname[260] = {0};
+			{
+				int j = 0;
+				int i = 5;
+				while(argv[2][i]!='\0')
+				{
+					iname[j] = argv[2][i];
+					i++;
+					j++;
+				}
+			}
+			{
+				int j = 0;
+				int i = 6;
+				while(argv[3][i]!='\0')
+				{
+					oname[j] = argv[3][i];
+					i++;
+					j++;
+				}
+			}
+			//			ifstream cfile;
+			cfile.open("code.txt");
+			if(cfile.is_open())
+			{
+				decode(iname, oname);
+				return 0;
+			}
+			else
+			{
+				printf("Couldn't find code.txt!\nPlease run crypt -generate first.\nExiting...");
+				return 0;
+			}
+		default:
+			printf("Invalid runtime argument!\nExiting...");
+			return 0;
 	}
 	system("pause");
-	return 0;
 }
