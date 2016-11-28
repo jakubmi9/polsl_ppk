b8ab9a1981f15ef5eaa2b2dd9dcead904015970b
Jakub Misiura <jakubmi9@gmail.com>
2016-11-28 11:04:32 +0100

    completed zad3; began zad2

--- Temat 07/README.md
+++ Temat 07/README.md
@@ -4,12 +4,13 @@
 2) zapisac elementy do pliku binarnego
 3) otworzyc i wyswietlic plik na ekranie
 ## Zad.2
-1) wypelnic tablice strukturami punkt
-2) zapisac elementy do pliku tekstowego
+1) ~~wypelnic tablice strukturami punkt~~
+2) ~~zapisac elementy do pliku tekstowego~~
 3) otworzyc i wyswietlic plik na ekranie
-## Zad.3
-1) na wejscie programu przyjac plik tekstowy
-2) odczytac go i utworzyc statystyke liter w tym pliku
+## ~~Zad.3~~
+1) ~~na wejscie programu przyjac plik tekstowy~~
+2) ~~odczytac go i utworzyc statystyke liter w tym pliku~~
+3) ~~wypisac statystyke na ekran~~
 ## Zad.4
 1) na wejscie programu przyjąć dwa pliki z liczbami całkowitymi
 2) pliki te są posortowane
--- Temat 07/Temat 07.cpp
+++ Temat 07/Temat 07.cpp
@@ -19,16 +19,74 @@ void zad1()
 	{
 		for(int j = 0; j < 5; j++)
 		{
-			tab[i][j] = rand();
+			tab[i][j].x = rand();
+			tab[i][j].y = rand();
 		}
 	}
 	ofstream o;
-	o.open("binary",ios::binary);
+	o.open("zad1.bin", ios::binary);
+	//do something...
+}
+//==============================================================================
+void zad2()
+{
+	srand(static_cast<unsigned int>(time(0)));
+	punkt tab[5][5];
+	for(int i = 0; i < 5; i++)
+	{
+		for(int j = 0; j < 5; j++)
+		{
+			tab[i][j].x = rand();
+			tab[i][j].y = rand();
+		}
+	}
+	ofstream o;
+	o.open("zad2.txt");
+	for(int i = 0; i < 5; i++)
+	{
+		for(int j = 0; j < 5; j++)
+		{
+			o << tab[i][j].x << ' ' << tab[i][j].y << endl;
+		}
+	}
+	o.close();
+	ifstream i;
+	i.open("zad2.txt");
+	for(int i = 0; i < 5; i++)
+	{
+		/*for(int j = 0; j < 5; j++)
+		{
+			int x;
+			int y;
+			i >> x >> y;
+			cout << "punkt " << i << ',' << j << "=(" << x << ',' << y << ")\n";
+		}*/
+	}
+}
+//==============================================================================
+void zad3(int argc, char* argv[])
+{
+	ifstream i;
+	i.open(argv[1]);
+	unsigned int tab[128] = {0};
+	char tmp;
+	while(i >> tmp)
+		tab[static_cast<unsigned int>(tmp)]++;
+	for(int i = 32; i < 127; i++)
+	{
+		cout << static_cast<char>(i) << '=' << tab[i] << endl;
+	}
+}
+//==============================================================================
+void zad4()
+{
 
 }
 //==============================================================================
-int main()
+int main(int argc, char* argv[])
 {
+	zad2();
+	zad3(argc, argv);
 	system("pause");
 }
 //==============================================================================
