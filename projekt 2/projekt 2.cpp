21b0c97081ae1da653d8d18a9857de9bb4aa3933
Jakub Misiura <jakubmi9@gmail.com>
2016-11-29 00:19:28 +0100

    began major ux overhaul

--- projekt 2/projekt 2.cpp
+++ projekt 2/projekt 2.cpp
@@ -12,48 +12,64 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	bool DEBUG = 1;
+	const string g = "libXpress Library Management System alpha\nLicensed under GNU GPLv3";
+	const string p = "libxpress";
+	sh::shell *main = new sh::shell(g, p);
 	try
 	{
-		//error when run w/out args
-		if(argc == 1)
+		while(true)
 		{
-			throw new NoArgumentException;
+			main->prompt();
+			if(main->command[0] == "exit")
+			{
+				delete main;
+				if(DEBUG) system("pause"); return 0;
+			}
+			else if(main->command[0] == "status")
+			{
+				//print current status of the library system
+			}
 		}
-		//print current status of the library system
-		else if(!strcmp(argv[1], "status"))
-		{
-			if(DEBUG) system("pause"); else return 0;
-		}
-		//lists books or users
-		else if(!strcmp(argv[1], "list"))
-		{
-			if(DEBUG) system("pause"); else return 0;
-		}
-		//adds an item to the database
-		else if(!strcmp(argv[1], "add"))
-		{
-			if(DEBUG) system("pause"); else return 0;
-		}
-		//edit an item
-		else if(!strcmp(argv[1], "edit"))
-		{
-			if(DEBUG) system("pause"); else return 0;
-		}
-		//deletes item from the database
-		else if(!strcmp(argv[1], "delete"))
-		{
-			if(DEBUG) system("pause"); else return 0;
-		}
-		//borrows a book to a user
-		else if(!strcmp(argv[1], "borrow"))
-		{
-			if(DEBUG) system("pause"); else return 0;
-		}
-		//error on non specified argument
-		else
-		{
-			throw new InvalidArgumentException;
-		}
+		////error when run w/out args
+		//if(argc == 1)
+		//{
+		//	throw new NoArgumentException;
+		//}
+		////print current status of the library system
+		//else if(!strcmp(argv[1], "status"))
+		//{
+		//	if(DEBUG) system("pause"); else return 0;
+		//}
+		////lists books or users
+		//else if(!strcmp(argv[1], "list"))
+		//{
+		//	if(DEBUG) system("pause"); else return 0;
+		//}
+		////adds an item to the database
+		//else if(!strcmp(argv[1], "add"))
+		//{
+		//	if(DEBUG) system("pause"); else return 0;
+		//}
+		////edit an item
+		//else if(!strcmp(argv[1], "edit"))
+		//{
+		//	if(DEBUG) system("pause"); else return 0;
+		//}
+		////deletes item from the database
+		//else if(!strcmp(argv[1], "delete"))
+		//{
+		//	if(DEBUG) system("pause"); else return 0;
+		//}
+		////borrows a book to a user
+		//else if(!strcmp(argv[1], "borrow"))
+		//{
+		//	if(DEBUG) system("pause"); else return 0;
+		//}
+		////error on non specified argument
+		//else
+		//{
+		//	throw new InvalidArgumentException;
+		//}
 	}
 	catch(NoArgumentException *exp)
 	{
