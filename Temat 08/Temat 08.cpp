26c3935ca0777b9590be4985df73dd7e57d4bd78
Jakub Misiura <jakubmi9@gmail.com>
2016-12-05 11:30:42 +0100

    created element structure, and Insert function

--- Temat 08/Temat 08.cpp
+++ Temat 08/Temat 08.cpp
@@ -3,9 +3,28 @@
 #include "stdafx.h"
 using namespace std;
 //==============================================================================
+struct element
+{
+	int Data;
+	element *NextElement;
+};
+//==============================================================================
+void InsertAtBeginning(element *&HEAD, int Data)
+{
+	element *NewElement = new element;
+	NewElement->Data = Data;
+	NewElement->NextElement = HEAD;
+	HEAD = NewElement;
+}
+//==============================================================================
 int main()
 {
-
+	element *List;
+	List = new element;
+	List->Data = 5;
+	InsertAtBeginning(List, 25);
+	InsertAtBeginning(List, 125);
+	cout << List->NextElement->NextElement->Data;
 	system("pause");
 }
 //==============================================================================
