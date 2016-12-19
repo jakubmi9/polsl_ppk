033c82c875aefc821295a09e755c110c9cbcc82c
Jakub Misiura <jakubmi9@gmail.com>
2016-12-19 10:36:40 +0100

    struct Tree; testing routine; main() structure

--- Temat 10/Temat 10.cpp
+++ Temat 10/Temat 10.cpp
@@ -1,11 +1,43 @@
 // Temat 10.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
+//==============================================================================
+struct Tree
+{
+	int Value = 0;
+	Tree* LeftChild = nullptr;
+	Tree* RightChild = nullptr;
+};
+//==============================================================================
+void Insert(Tree *&ROOT, int Value)
+{
 
+}
+//==============================================================================
+void PrintTree()
+{
 
+}
+//==============================================================================
+void DeleteTree()
+{
+
+}
+//==============================================================================
 int main()
 {
-    return 0;
+	Tree* ROOT = new Tree;
+	ROOT->Value = 20;
+	Insert(ROOT, 10);
+	Insert(ROOT, 50);
+	Insert(ROOT, 5);
+	Insert(ROOT, 60);
+	Insert(ROOT, 8);
+	Insert(ROOT, 30);
+	Insert(ROOT, 25);
+	Insert(ROOT, 40);
+	PrintTree();
+	DeleteTree();
+	system("pause");
 }
-
+//==============================================================================
